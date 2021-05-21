ddb8ff794584cfefe8bef7e88c72deeb9e973b06
hamberg1 <83711884+hamberg1@users.noreply.github.com>
2021-05-21 14:21:47 +0530

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,16 @@
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
+  const subject = event.queryStringParameters.serial || 'World'
+  const url = "https://api.github.com/repos/hamberg1/md_blog/contents/contents";
+  const key 
+  fetch(url)
+  .then(re=>re.json())
+  .then(jl=>{
+	key = jl.find(o => o.name.includes(subject));
+
+	console.log(key);
+	})
   return {
     statusCode: 200,
-    body: `Hello ${subject}!`
+    body: `</h1> ${key}</h1>`
   }
 }
