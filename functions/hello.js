d1a148de4c6ba8ac1f885e1286603ad393e15a94
hamberg1 <83711884+hamberg1@users.noreply.github.com>
2021-05-22 12:44:53 +0530

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,18 +1,8 @@
-const fetch = require("node-fetch");
-const url = "https://api.github.com/repos/hamberg1/md_blog/contents/contents";
-
 exports.handler = async event => {
-  const subject = event.queryStringParameters.serial || 'World'
-  const key 
-  fetch(url)
-  .then(re=>re.json())
-  .then(jl=>{
-	key = jl.find(o => o.name.includes(subject));
-
-	console.log(key);
-	})
+  const subject = event.queryStringParameters.name || 'World'
+  //const url = "https://api.github.com/repos/hamberg1/md_blog/contents/contents";
   return {
     statusCode: 200,
-    body: `</h1> ${key}</h1>`
+    body: `</h1> ${subject}</h1>`
   }
 }
